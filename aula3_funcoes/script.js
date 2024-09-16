cd25bbd63204f00a2623952b008abc1d2434c553
GuilhermeSilvav <127443339+GuilhermeSilvav@users.noreply.github.com>
2024-09-16 20:57:22 -0300

    Add files via upload

---
+++ aula3_funcoes/script.js
@@ -0,0 +1,11 @@
+//criar funções 
+
+//utilizando botão
+function nome_funcao(){
+    alert ("Primeira função!")
+}
+
+function somar(a, b){
+    var total = a + b;
+    alert("valor soma: "+ total);
+}
